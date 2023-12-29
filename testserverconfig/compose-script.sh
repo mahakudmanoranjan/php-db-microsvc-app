bf58c9bf112e969de78fea98db0f180b4c70e23f
Manoranjan Mahakud <mrmahakud95@gmail.com>
2023-12-29 11:28:01 +0530

    docker-compose modified

--- testserverconfig/compose-script.sh
+++ testserverconfig/compose-script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo DOCKER_PHP_IMAGE=$1 docker-compose -f /home/ec2-user/testserverconfig/docker-compose.yml up -d
+sudo DOCKER_PHP_IMAGE=$1 /usr/local/bin/docker-compose -f /home/ec2-user/testserverconfig/docker-compose.yml up -d
